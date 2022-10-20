cf882b2e585c5754ffa949433eb0cabca19be5a0
Ash530888 <aishah.s.qureshi@gmail.com>
2022-10-20 15:15:32 +0100

    first version of compatibility score calculations

--- src/MM_Algo.py
+++ src/MM_Algo.py
@@ -18,13 +18,28 @@ def main(argv):
     mentor = args.mentor_sheet
 
 
-    # Read excel sheets
-    columns = ["firstName", "surname:", "emailAddress:", "contactNumber:", "studentID", "gender", "selfDescribeGender", "academicSchool", "courseTitle", "yearOfStudy",	"ethnicity", "otherEthnic", "disabled", "preferredGender", "other", "typeMentor", 	"industry_job", "otherPreferences", "why", "bursary"]
+    # Read excel sheets with 
+    columns = ["firstname", "surname", "emailAddress", "contactNumber", "studentID", "gender", "selfDescribeGender", "academicSchool", "courseTitle", "yearOfStudy",	"ethnicity", "otherEthnic", "disabled", "preferredGender", "other", "typeMentor", 	"industry_job", "otherPreferences", "why", "bursary"]
     menteeData = pd.read_excel(filepath, sheet_name = mentee, header = None, names = columns)
+    menteeData.drop(index=menteeData.index[0], axis=0, inplace=True)
 
-    columns = ["firstName", "surname:", "emailAddress:", "homeAddress", "contactNumber:", "gender", "selfDescribeGender", "formerQMUL", "qualifications",	"ethnicity", "otherEthnic", "disabled", "preferredGender", "other", "menteeSchools", "otherPreferences", "jobTitle", "company", "industry", "jobDesc", "skills_exp", "dataCollection"]
+    columns = ["firstname", "surname", "emailAddress", "homeAddress", "contactNumber", "gender", "selfDescribeGender", "formerQMUL", "qualifications",	"ethnicity", "otherEthnic", "disabled", "preferredGender", "other", "menteeSchools", "otherPreferences", "jobTitle", "company", "industry", "jobDesc", "skills_exp", "dataCollection"]
     mentorData = pd.read_excel(filepath, sheet_name = mentor, header = None, names = columns)
+    mentorData.drop(index=mentorData.index[0], axis=0, inplace=True)
 
+    mentorNames = []
+
+    print(mentorData["firstname"])
+    for i, x in enumerate(mentorData["firstname"]):
+        mentorNames.append(x+" "+mentorData.iloc[i]["surname"])
+
+    
+    matches = pd.DataFrame(columns = mentorNames)
+
+    print(matches)
 
 if __name__=="__main__":
     main(sys.argv)
+
+
+
